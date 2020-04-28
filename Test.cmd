88a5fe8f8f10e8e6eb67ed42a9ec597b54e2fd56
Martin Dubois <mdubois@kms-quebec.com>
2020-04-28 19:04:05 -0400

    0.0.1_Windows

--- Test.cmd
+++ Test.cmd
@@ -14,11 +14,25 @@ rem ===== Verification ======================================================
 
 rem ===== Execution =========================================================
 
+Debug\HILib_Test.exe
+if ERRORLEVEL 1 (
+	echo ERROR  Debug\HILib_Test.exe  failed - %ERRORLEVEL%
+	pause
+	exit /B 5
+)
+
 Debug\GenDoc.exe
 if ERRORLEVEL 1 (
 	echo ERROR  Debug\GenDoc.exe  failed - %ERRORLEVEL%
 	pause
 	exit /B 10
+)
+
+x64\Debug\HILib_Test.exe
+if ERRORLEVEL 1 (
+	echo ERROR  x64\Debug\HILib_Test.exe  failed - %ERRORLEVEL%
+	pause
+	exit /B 15
 )
 
 x64\Debug\GenDoc.exe
@@ -28,11 +42,25 @@ if ERRORLEVEL 1 (
 	exit /B 20
 )
 
+Release\HILib_Test.exe
+if ERRORLEVEL 1 (
+	echo ERROR  Release\HILib_Test.exe  failed - %ERRORLEVEL%
+	pause
+	exit /B 25
+)
+
 Release\GenDoc.exe
 if ERRORLEVEL 1 (
 	echo ERROR  Release\GenDoc.exe  failed - %ERRORLEVEL%
 	pause
 	exit /B 30
+)
+
+x64\Release\HILib_Test.exe
+if ERRORLEVEL 1 (
+	echo ERROR  x64\Release\HILib_Test.exe  failed - %ERRORLEVEL%
+	pause
+	exit /B 35
 )
 
 x64\Release\GenDoc.exe
